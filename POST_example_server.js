aecedcac67a3e5ce60c9df9fdd424eb4a742ede6
Dave Westerveld <Dave.Westerveld@D2L.com>
2018-09-21 14:05:22 -0400

    Use the correct database

--- POST_example_server.js
+++ POST_example_server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('find_the_500_db.json')
+const router = jsonServer.router('POST_example_db.json')
 const middlewares = jsonServer.defaults()
 
 function compareArrays(arr1,arr2){
